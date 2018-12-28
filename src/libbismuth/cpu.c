292bcc724fc4161d1a683579404b7ddb5e5673e8
Nax <max@bacoux.com>
2018-12-28 04:02:28 +0100

    Typed every opcode

--- src/libbismuth/cpu.c
+++ src/libbismuth/cpu.c
@@ -405,6 +405,390 @@ void biRunCycles(BiContext* ctx, size_t cycles)
         case OP_ADC_AXL:
             TRAP;
             break;
+        case OP_BRA_R:
+            TRAP;
+            break;
+        case OP_STA_DXI:
+            TRAP;
+            break;
+        case OP_BRL_RL:
+            TRAP;
+            break;
+        case OP_STA_DS:
+            TRAP;
+            break;
+        case OP_STY_D:
+            TRAP;
+            break;
+        case OP_STA_D:
+            TRAP;
+            break;
+        case OP_STX_D:
+            TRAP;
+            break;
+        case OP_STA_DL:
+            TRAP;
+            break;
+        case OP_DEY:
+            TRAP;
+            break;
+        case OP_BIT_IMM:
+            TRAP;
+            break;
+        case OP_TXA:
+            TRAP;
+            break;
+        case OP_PHB_S:
+            TRAP;
+            break;
+        case OP_STY_A:
+            TRAP;
+            break;
+        case OP_STA_A:
+            TRAP;
+            break;
+        case OP_STX_A:
+            TRAP;
+            break;
+        case OP_STA_AL:
+            TRAP;
+            break;
+        case OP_BCC_R:
+            TRAP;
+            break;
+        case OP_STA_DYI:
+            TRAP;
+            break;
+        case OP_STA_DI:
+            TRAP;
+            break;
+        case OP_STA_DYSI:
+            TRAP;
+            break;
+        case OP_STY_DX:
+            TRAP;
+            break;
+        case OP_STA_DX:
+            TRAP;
+            break;
+        case OP_STX_DY:
+            TRAP;
+            break;
+        case OP_STA_DYL:
+            TRAP;
+            break;
+        case OP_TYA:
+            TRAP;
+            break;
+        case OP_STA_AY:
+            TRAP;
+            break;
+        case OP_TXS:
+            TRAP;
+            break;
+        case OP_TXY:
+            TRAP;
+            break;
+        case OP_STZ_A:
+            TRAP;
+            break;
+        case OP_STA_AX:
+            TRAP;
+            break;
+        case OP_STZ_AX:
+            TRAP;
+            break;
+        case OP_STA_AXL:
+            TRAP;
+            break;
+        case OP_LDY_MM:
+            TRAP;
+            break;
+        case OP_LDA_DXI:
+            TRAP;
+            break;
+        case OP_LDX_IMM:
+            TRAP;
+            break;
+        case OP_LDA_DS:
+            TRAP;
+            break;
+        case OP_LDY_D:
+            TRAP;
+            break;
+        case OP_LDA_D:
+            TRAP;
+            break;
+        case OP_LDX_D:
+            TRAP;
+            break;
+        case OP_LDA_DL:
+            TRAP;
+            break;
+        case OP_TAY:
+            TRAP;
+            break;
+        case OP_LDA_IMM:
+            TRAP;
+            break;
+        case OP_TAX:
+            TRAP;
+            break;
+        case OP_PLB_S:
+            TRAP;
+            break;
+        case OP_LDY_A:
+            TRAP;
+            break;
+        case OP_LDA_A:
+            TRAP;
+            break;
+        case OP_LDX_A:
+            TRAP;
+            break;
+        case OP_LDA_AL:
+            TRAP;
+            break;
+        case OP_BCS_R:
+            TRAP;
+            break;
+        case OP_LDA_DYI:
+            TRAP;
+            break;
+        case OP_LDA_DI:
+            TRAP;
+            break;
+        case OP_LDA_DYSI:
+            TRAP;
+            break;
+        case OP_LDY_DX:
+            TRAP;
+            break;
+        case OP_LDA_DX:
+            TRAP;
+            break;
+        case OP_LDX_DY:
+            TRAP;
+            break;
+        case OP_LDA_DYL:
+            TRAP;
+            break;
+        case OP_CLV:
+            TRAP;
+            break;
+        case OP_LDA_AY:
+            TRAP;
+            break;
+        case OP_TSX:
+            TRAP;
+            break;
+        case OP_TYX:
+            TRAP;
+            break;
+        case OP_LDY_AX:
+            TRAP;
+            break;
+        case OP_LDA_AX:
+            TRAP;
+            break;
+        case OP_LDX_AY:
+            TRAP;
+            break;
+        case OP_LDA_AXL:
+            TRAP;
+            break;
+        case OP_CPY_IMM:
+            TRAP;
+            break;
+        case OP_CMP_DXI:
+            TRAP;
+            break;
+        case OP_REP_IMM:
+            TRAP;
+            break;
+        case OP_CMP_DS:
+            TRAP;
+            break;
+        case OP_CPY_D:
+            TRAP;
+            break;
+        case OP_CMP_D:
+            TRAP;
+            break;
+        case OP_DEC_D:
+            TRAP;
+            break;
+        case OP_CMP_DL:
+            TRAP;
+            break;
+        case OP_INY:
+            TRAP;
+            break;
+        case OP_CMP_IMM:
+            TRAP;
+            break;
+        case OP_DEX:
+            TRAP;
+            break;
+        case OP_WAI:
+            TRAP;
+            break;
+        case OP_CPY_A:
+            TRAP;
+            break;
+        case OP_CMP_A:
+            TRAP;
+            break;
+        case OP_DEC_A:
+            TRAP;
+            break;
+        case OP_CMP_AL:
+            TRAP;
+            break;
+        case OP_BNE_R:
+            TRAP;
+            break;
+        case OP_CMP_DYI:
+            TRAP;
+            break;
+        case OP_CMP_DI:
+            TRAP;
+            break;
+        case OP_CMP_DYSI:
+            TRAP;
+            break;
+        case OP_PEI_S:
+            TRAP;
+            break;
+        case OP_CMP_DX:
+            TRAP;
+            break;
+        case OP_DEC_DX:
+            TRAP;
+            break;
+        case OP_CMP_DYL:
+            TRAP;
+            break;
+        case OP_CLD:
+            TRAP;
+            break;
+        case OP_CMP_AY:
+            TRAP;
+            break;
+        case OP_PHX_S:
+            TRAP;
+            break;
+        case OP_STP:
+            TRAP;
+            break;
+        case OP_JML_AI:
+            TRAP;
+            break;
+        case OP_CMP_AX:
+            TRAP;
+            break;
+        case OP_DEC_AX:
+            TRAP;
+            break;
+        case OP_CML_AXL:
+            TRAP;
+            break;
+        case OP_CPX_IMM:
+            TRAP;
+            break;
+        case OP_SBC_DXI:
+            TRAP;
+            break;
+        case OP_SEP_IMM:
+            TRAP;
+            break;
+        case OP_SBC_DS:
+            TRAP;
+            break;
+        case OP_CPX_D:
+            TRAP;
+            break;
+        case OP_SBC_D:
+            TRAP;
+            break;
+        case OP_INC_D:
+            TRAP;
+            break;
+        case OP_SBC_DL:
+            TRAP;
+            break;
+        case OP_INX:
+            TRAP;
+            break;
+        case OP_SBC_IMM:
+            TRAP;
+            break;
+        case OP_NOP:
+            TRAP;
+            break;
+        case OP_XBA:
+            TRAP;
+            break;
+        case OP_CPX_A:
+            TRAP;
+            break;
+        case OP_SBC_A:
+            TRAP;
+            break;
+        case OP_INC_A:
+            TRAP;
+            break;
+        case OP_SBC_AL:
+            TRAP;
+            break;
+        case OP_BEQ_R:
+            TRAP;
+            break;
+        case OP_SBC_DYI:
+            TRAP;
+            break;
+        case OP_SBC_DI:
+            TRAP;
+            break;
+        case OP_SBC_DYSI:
+            TRAP;
+            break;
+        case OP_PEA_S:
+            TRAP;
+            break;
+        case OP_SBC_DX:
+            TRAP;
+            break;
+        case OP_INC_DX:
+            TRAP;
+            break;
+        case OP_SBC_DYL:
+            TRAP;
+            break;
+        case OP_SED:
+            TRAP;
+            break;
+        case OP_SBC_AY:
+            TRAP;
+            break;
+        case OP_PLX_S:
+            TRAP;
+            break;
+        case OP_XCE:
+            TRAP;
+            break;
+        case OP_JSR_AXI:
+            TRAP;
+            break;
+        case OP_SBC_AX:
+            TRAP;
+            break;
+        case OP_INC_AX:
+            TRAP;
+            break;
+        case OP_SBC_AXL:
+            TRAP;
+            break;
         }
     }
 
